37cddb0be386ee6b5555d1fc289f93a184a12625
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-06-04 22:05:17 -0300

    configura prisma client e adiciona rota para criar usuarios

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
-import fastify from "fastify"
+import fastify, { FastifyReply, FastifyRequest } from "fastify"
+import { prisma } from "./lib/prisma"
+import { z } from 'zod'
 
 export const app = fastify()
+
+app.post("/users", async (request: FastifyRequest, reply: FastifyReply) => {
+  const RegisterBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z.string().min(6, { message: "Sua senha deve conter no mínimo 6 caracteres" }),
+    passwordConfirm: z.string().min(6, { message: "Sua senha deve conter no mínimo 6 caracteres" })
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas devem ser iguais",
+    path: ["passwordConfirm"]
+  })
+
+  const { name, email, password } = RegisterBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password
+    }
+  })
+
+  reply.status(201).send()
+})
