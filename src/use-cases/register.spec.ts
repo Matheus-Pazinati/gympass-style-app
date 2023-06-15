16440a8273447faf01bfaec0c1375f2e5d53c3cf
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-06-14 22:28:04 -0300

    cria teste para verificar se a aplicacao permite a criacao de dois usuarios com o mesmo email

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-user-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   it("should hash user password uppon register", async () => {
@@ -20,5 +21,24 @@ describe('Register Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it("should not be able to register with same email twice", async () => {
+    const inMemoryRepository = new InMemoryUserRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryRepository)
+
+    await registerUseCase.execute({
+      email: "johndoe@email.com",
+      name: "John Doe",
+      password: "123456"
+    })
+
+    expect(async () => {
+      await registerUseCase.execute({
+        email: "johndoe@email.com",
+        name: "John Doe",
+        password: "123456"
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
