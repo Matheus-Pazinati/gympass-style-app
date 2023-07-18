aa57326158479ff770a61e915850c382fe6bc620
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-07-17 22:16:21 -0300

    finaliza configuracao do ambiente de testes end-to-end

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,37 @@
-import { Environment } from "vitest"
+import "dotenv/config"
+import { randomUUID } from "node:crypto"
+import { execSync } from 'node:child_process'
+import type { Environment } from "vitest"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+function createTestDatabaseURL(schema: string) {
+  const databaseURL = process.env.DATABASE_URL
+  if (!databaseURL) {
+    throw new Error("Set a DATABASE_URL environment variable")
+  }
+
+  const url = new URL(databaseURL)
+  url.searchParams.set("schema", schema)
+
+  return url.toString()
+} 
 
 export default <Environment> {
   name: 'prisma',
   async setup() {
-    console.log("Executou")
+    const schema = randomUUID()
+    const testDatabaseURL = createTestDatabaseURL(schema)
+    process.env.DATABASE_URL = testDatabaseURL
+
+    execSync("npx prisma migrate deploy")
 
     return {
-      teardown() {}
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+        await prisma.$disconnect()
+      }
     }
   },
 }
