ba1e3711e4ea7a6de87f223982d0784e30b1f405
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-07-19 19:20:56 -0300

    cria teste e2e para validar o registro do usuario

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,25 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, it } from "vitest";
+import { app } from '../../app'
+import request from 'supertest'
 
-test("test example", async () => {})
+describe("Register E2E", async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+
+  it("should be able to register a user", async () => {
+    await request(app.server).post('/users')
+    .send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+      passwordConfirm: "123456"
+    })
+    .expect(201)
+  })
+})
