b290e1e5f73e5a0b63500da55374f9c58f3cb75c
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-07-19 21:56:22 -0300

    cria teste para validar a rota de perfil do usuario

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,41 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from '../../app'
+import request from 'supertest'
+
+describe("Profile E2E Test", async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it("should be able to get user profile", async () => {
+
+    await request(app.server).post('/users')
+    .send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+      passwordConfirm: "123456"
+    })
+
+    const authenticationResponse = await request(app.server).post('/sessions')
+    .send({
+      email: "johndoe@example.com",
+      password: "123456"
+    })
+
+    const { token } = authenticationResponse.body
+
+    const response = await request(app.server).get('/me')
+    .set('Authorization', `Bearer ${token}`)
+    .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual(expect.objectContaining({
+      name: "John Doe"
+    }))
+  })
+})
